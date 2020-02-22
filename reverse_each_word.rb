85cb21bfcdb17cf6751bf930d4be481f44fc20c6
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-22 05:37:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,4 +10,12 @@ def reverse_each_word(string)
   new_string.join(" ")
 end
 
-def 
+def reverse_each_word(string)
+  #binding.pry
+  new_string = []
+  words = string.split
+  words.collect do |word|
+    new_string.push(word.reverse)
+  end
+  new_string.join(" ")
+end
